f01a7d835c54fed2ed00373016b6346cc1977392
mindayy <163283800+mindayy@users.noreply.github.com>
2024-07-23 21:14:00 +0800

    added navbar to profile page

--- main-page/src/App.js
+++ main-page/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Products from './Pages/Products';
 import Auth from './Components/Auth/Auth';
-import ProfilePage from './Components/ProfilePage/ProfilePage';
+import Profile from './Pages/Profile';
 import Search from './Pages/Search'; 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/" element={<Home/>} />
           <Route path="/products" element={<Products/>} />
           <Route path="/auth" element={<Auth/>} />
-          <Route path="/profile" element={<ProfilePage/>} />
+          <Route path="/profile" element={<Profile/>} />
           <Route path="/search" element={<Search />} />
         </Routes>
       </div>
---
+++ main-page/src/Pages/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar';
+import ProfilePage from '../Components/ProfilePage/ProfilePage'
+
+const Profile = () => {
+    return (
+        <div>
+            <Navbar/>
+            <ProfilePage/>
+        </div>
+    )
+}
+
+export default Profile
