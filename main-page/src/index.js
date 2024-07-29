c7d5fd73bef94999eaaeaaa8645dcde8881a0cff
hngkxn <hngkxn@users.noreply.github.com>
2024-07-29 11:11:39 +0800

    Edited file name error

--- main-page/src/index.js
+++ main-page/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { WishlistProvider } from './Components/WishlistContext/WishlistContext';
 import { UserProvider } from './Components/UserContext/UserContext'; // Import UserProvider
 
